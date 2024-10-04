0b9cc3136fd1eb3fc1dd00b7617af3a6b2d51cce
crossedbells <147357688+crossedbells@users.noreply.github.com>
2024-10-04 20:57:41 -0300

    Modelo MVC
    Arquivos com apenas a estrutura do modelo MVC pronta

---
+++ VIDA_AGGP_Controller.h
@@ -0,0 +1,7 @@
+//VIDA_Ativi02 - Projeto Jogo da Vida - etapa 1
+//02/10/2024 - Grupo: AGGP
+
+//Alexandre Maciano de Oliveira 
+//Gabriel Manganiello Terra
+//Gabriel Mechi Lima
+//Pedro Marinho Machado
